5b3da7fdd23186c960903c7bb129912e1b99eff2
bahaha <clementcheng56@gmail.com>
2022-11-26 21:44:54 +0800

    feat(auth-center): issue jws token with non-encrypted rsa256 pem (#5)

---
+++ knocknut-auth-center/src/test/java/dev/claycheng/authcenter/HMacJwtTokenOAuth2Spec.groovy
@@ -0,0 +1,122 @@
+package dev.claycheng.authcenter
+
+import dev.claycheng.api.AuthToken
+import dev.claycheng.api.CommonResult
+import dev.claycheng.api.enums.MemberStatus
+import dev.claycheng.knocknut.api.feign.user.Member
+import dev.claycheng.knocknut.api.feign.user.MemberFeignApi
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.provider.ClientDetailsService
+import org.springframework.security.oauth2.provider.client.BaseClientDetails
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
+import org.springframework.web.util.UriComponentsBuilder
+import spock.lang.Specification
+
+import javax.sql.DataSource
+import java.nio.charset.StandardCharsets
+import java.time.Duration
+
+@TestPropertySource(properties = "knocknut.auth.jwt.enabled=true")
+@TestPropertySource(properties = "knocknut.auth.jwt.algo=hashing")
+@TestPropertySource(properties = "knocknut.auth.jwt.secret=clay@knocknut_VSVn5YJXx3jwWCpVHRtXgZnQ997n")
+@TestConfiguration
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HMacJwtTokenOAuth2Spec extends Specification {
+
+
+    @Autowired
+    TestRestTemplate testRestTemplate
+
+    BaseClientDetails integrationClient
+
+    def jwtParser = Jwts.parserBuilder()
+        .setSigningKey(Keys.hmacShaKeyFor("clay@knocknut_VSVn5YJXx3jwWCpVHRtXgZnQ997n".getBytes(StandardCharsets.UTF_8)))
+        .build()
+
+    @SpringBean
+    DataSource dataSource = Mock()
+
+    @SpringBean
+    PasswordEncoder plainTextEncoder = NoOpPasswordEncoder.instance
+
+    @SpringBean
+    MemberFeignApi memberApi = Stub(MemberFeignApi) {
+        findByUsername(_) >> {
+            def member = new Member()
+            member.username = 'clay'
+            member.password = '123123123'
+            member.status = MemberStatus.ENABLED
+            CommonResult.success(member)
+        }
+    }
+
+    @SpringBean(name = "clientDetails")
+    ClientDetailsService integrationClientStub = Stub(ClientDetailsService) {
+        loadClientByClientId('integration-client') >> { _ -> integrationClient }
+    }
+
+
+    def "issue JWS with secret hashing"() {
+        given:
+        integrationClient = trustClient([
+            clientId  : "integration-client", clientSecret: "#dev#clay",
+            grantTypes: ["password", "refresh_token"], scopes: ["read", "write"]])
+
+        when:
+        def response = requestToken([
+            auth  : [clientId: "integration-client", clientSecret: "#dev#clay"],
+            params: [grant_type: "password", scope: "read write", username: "clay", password: "123123123"]
+        ])
+
+        then:
+        response.statusCode == HttpStatus.OK
+        AuthToken authToken = response.body
+        String jwtToken = authToken.access_token
+        Jws<Claims> claims = jwtParser.parseClaimsJws(jwtToken)
+        with(claims.body) {
+            user_name == 'clay'
+            client_id == 'integration-client'
+            scope == ["read", "write"]
+        }
+    }
+
+
+    ResponseEntity<AuthToken> requestToken(Map props) {
+        Map<String, String> auth = props.auth ?: [:]
+        Map<String, String> params = props.params ?: [:]
+        def uriBuilder = UriComponentsBuilder.fromUriString("/oauth/token")
+        params.each { uriBuilder.queryParam(it.key, it.value) }
+
+        testRestTemplate.withBasicAuth(auth.clientId, auth.clientSecret)
+            .exchange(uriBuilder.build().toUri(),
+                HttpMethod.POST,
+                HttpEntity.EMPTY,
+                AuthToken.class)
+    }
+
+    BaseClientDetails trustClient(Map<String, Object> props) {
+        def client = new BaseClientDetails()
+        client.clientId = props.clientId
+        client.clientSecret = props.clientSecret
+        client.authorizedGrantTypes = props.grantTypes as List<String>
+        client.scope = props.scopes as List<String>
+        client.accessTokenValiditySeconds = Duration.ofDays(1).toSeconds().intValue()
+        client
+    }
+}
