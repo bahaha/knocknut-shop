87f6c3975960be7677b672e4db2e2d1cad9bb0c6
Cheng Che <clementcheng56@gmail.com>
2022-12-07 22:34:26 +0800

    update description about secrurity

--- knocknut-auth-center/src/main/java/dev/claycheng/knocknut/config/InMemoryTokenStore.java
+++ knocknut-auth-center/src/main/java/dev/claycheng/knocknut/config/InMemoryTokenStore.java
@@ -1,5 +1,6 @@
 package dev.claycheng.knocknut.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -10,6 +11,11 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
  *
  * @author Clay Cheng
  */
+@ConditionalOnProperty(
+    prefix = "knocknut.auth.jwt",
+    name = "enabled",
+    havingValue = "false",
+    matchIfMissing = true)
 @Configuration
 public class InMemoryTokenStore {
   @Bean
