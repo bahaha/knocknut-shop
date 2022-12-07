a894e1fc109f67702cc54fe16a528e740f0fa3a3
Cheng Che <clementcheng56@gmail.com>
2022-12-08 01:09:33 +0800

    fix: undo the bad conflict merge

--- knocknut-auth-center/src/main/java/dev/claycheng/knocknut/config/InMemoryTokenStore.java
+++ knocknut-auth-center/src/main/java/dev/claycheng/knocknut/config/InMemoryTokenStore.java
@@ -2,8 +2,8 @@ package dev.claycheng.knocknut.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.stereotype.Component;
 
 /**
  * Register the InMemoryTokenStore from Spring as the token store, the default token store if no
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
     name = "enabled",
     havingValue = "false",
     matchIfMissing = true)
-@Component
+@Configuration
 public class InMemoryTokenStore {
 
   @Bean
